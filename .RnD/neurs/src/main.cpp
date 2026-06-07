cf88413d57f91ef095a7ac2f9d37296bf4bd165c
neu-rah <ruihfazevedo@gmail.com>
2026-06-07 04:55:56 +0000

    prepare for Chain simplification

--- .RnD/neurs/src/main.cpp
+++ .RnD/neurs/src/main.cpp
@@ -1,7 +1,8 @@
 #include <hapi/hapi.h>
-using hapi::Chain;
-using hapi::APIOf;
-using hapi::SameAs;
+// using hapi::Chain;
+// using hapi::APIOf;
+// using hapi::SameAs;
+using namespace hapi;
 
 #ifdef __AVR__
   #include <streamFlow.h>
@@ -41,112 +42,112 @@ struct C {
   };
 };
 
-// Variadic Logical Predicates ----------------------------
-template <typename P>
-struct Not {
-  template <typename O>
-  struct Check {
-    static constexpr const bool value{!P::template Check<O>::value};
-  };
-};
+// // Variadic Logical Predicates ----------------------------
+// template <typename P>
+// struct Not {
+//   template <typename O>
+//   struct Check {
+//     static constexpr const bool value{!P::template Check<O>::value};
+//   };
+// };
 
-template <typename... Ps>
-struct And {
-  template <typename O>
-  struct Check {
-    static constexpr const bool value{(Ps::template Check<O>::value && ... && true)};
-  };
-};
+// template <typename... Ps>
+// struct And {
+//   template <typename O>
+//   struct Check {
+//     static constexpr const bool value{(Ps::template Check<O>::value && ... && true)};
+//   };
+// };
 
-template <typename... Ps>
-struct Or {
-  template <typename O>
-  struct Check {
-    static constexpr const bool value{(Ps::template Check<O>::value || ... )};
-  };
-};
+// template <typename... Ps>
+// struct Or {
+//   template <typename O>
+//   struct Check {
+//     static constexpr const bool value{(Ps::template Check<O>::value || ... )};
+//   };
+// };
 
-// Monadic Channels (Concrete Control Metaprogramming) ----
-template<typename T>
-struct Left {
-  using Type = T;
-  template<typename O> struct Check { 
-    static constexpr bool value = std::is_same_v<O, Left<T>> || std::is_same_v<O, T>; 
-  };
-};
+// // Monadic Channels (Concrete Control Metaprogramming) ----
+// template<typename T>
+// struct Left {
+//   using Type = T;
+//   template<typename O> struct Check { 
+//     static constexpr bool value = std::is_same_v<O, Left<T>> || std::is_same_v<O, T>; 
+//   };
+// };
 
-template<typename T>
-struct Right {
-  using Type = T;
-  template<typename O> struct Check { 
-    static constexpr bool value = std::is_same_v<O, Right<T>> || std::is_same_v<O, T>; 
-  };
-};
+// template<typename T>
+// struct Right {
+//   using Type = T;
+//   template<typename O> struct Check { 
+//     static constexpr bool value = std::is_same_v<O, Right<T>> || std::is_same_v<O, T>; 
+//   };
+// };
 
-// Universal Inspection Tool ------------------------------
-template<template<typename...> class Wrapper>
-struct IsInstanceOf {
-  template<typename O> struct Check { 
-    static constexpr bool value{false}; 
-  };
-  template<typename... Args> struct Check<Wrapper<Args...>> { 
-    static constexpr bool value{true}; 
-  };
-};
+// // Universal Inspection Tool ------------------------------
+// template<template<typename...> class Wrapper>
+// struct IsInstanceOf {
+//   template<typename O> struct Check { 
+//     static constexpr bool value{false}; 
+//   };
+//   template<typename... Args> struct Check<Wrapper<Args...>> { 
+//     static constexpr bool value{true}; 
+//   };
+// };
 
-// Global Convenience Aliases for the Toolset
-struct IsLeft  : IsInstanceOf<Left> {};
-struct IsRight : IsInstanceOf<Right> {};
+// // Global Convenience Aliases for the Toolset
+// struct IsLeft  : IsInstanceOf<Left> {};
+// struct IsRight : IsInstanceOf<Right> {};
 
-// Categorization Transformation ---------------------------
-template<typename Q>
-struct Partition {
-  template<typename O>
-  struct Apply {
-    static constexpr bool value = Q::template Check<O>::value;
-    using Expr = std::conditional_t<value, Right<O>, Left<O>>;
-  };
-};
+// // Categorization Transformation ---------------------------
+// template<typename Q>
+// struct Partition {
+//   template<typename O>
+//   struct Apply {
+//     static constexpr bool value = Q::template Check<O>::value;
+//     using Expr = std::conditional_t<value, Right<O>, Left<O>>;
+//   };
+// };
 
-// Pure 1:1 Map Topology Walker ---------------------------
-template<typename F, typename O>
-struct Map {
-  using Expr = typename F::template Apply<O>::Expr;
-};
+// // Pure 1:1 Map Topology Walker ---------------------------
+// template<typename F, typename O>
+// struct Map {
+//   using Expr = typename F::template Apply<O>::Expr;
+// };
 
-template<typename F, typename... OO>
-struct Map<F, Chain<OO...>> {
-  using Expr = Chain<typename Map<F, OO>::Expr...>;
-};
+// template<typename F, typename... OO>
+// struct Map<F, Chain<OO...>> {
+//   using Expr = Chain<typename Map<F, OO>::Expr...>;
+// };
 
-// Conditional Extraction and Cleanup Engine --------------
-template<typename P, typename C, typename Enable = void> 
-struct FilterIf;
+// // Conditional Extraction and Cleanup Engine --------------
+// template<typename P, typename C, typename Enable = void> 
+// struct FilterIf;
 
-template<typename P> 
-struct FilterIf<P, Chain<>> { 
-  using Expr = Chain<>; 
-};
+// template<typename P> 
+// struct FilterIf<P, Chain<>> { 
+//   using Expr = Chain<>; 
+// };
 
-// 1. Case for Monadic Wrappers (Excludes Chains using SFINAE to prevent ambiguity)
-template<typename P, template<typename...> class Wrapper, typename T, typename... Args, typename... OO>
-struct FilterIf<P, Chain<Wrapper<T, Args...>, OO...>, 
-                std::enable_if_t<!std::is_same_v<Wrapper<T, Args...>, Chain<T, Args...>>>> {
-  static constexpr bool match = P::template Check<Wrapper<T, Args...>>::value;
+// // 1. Case for Monadic Wrappers (Excludes Chains using SFINAE to prevent ambiguity)
+// template<typename P, template<typename...> class Wrapper, typename T, typename... Args, typename... OO>
+// struct FilterIf<P, Chain<Wrapper<T, Args...>, OO...>, 
+//                 std::enable_if_t<!std::is_same_v<Wrapper<T, Args...>, Chain<T, Args...>>>> {
+//   static constexpr bool match = P::template Check<Wrapper<T, Args...>>::value;
   
-  using Expr = std::conditional_t<
-    match,
-    typename FilterIf<P, Chain<OO...>>::Expr::template App<T>,
-    typename FilterIf<P, Chain<OO...>>::Expr
-  >;
-};
+//   using Expr = std::conditional_t<
+//     match,
+//     typename FilterIf<P, Chain<OO...>>::Expr::template App<T>,
+//     typename FilterIf<P, Chain<OO...>>::Expr
+//   >;
+// };
 
-// 2. Case for Recursive Sub-Chains within the hardware tree
-template<typename P, typename... OO, typename... Rest>
-struct FilterIf<P, Chain<Chain<OO...>, Rest...>> {
-  using HeadFilter = typename FilterIf<P, Chain<OO...>>::Expr;
-  using Expr = typename FilterIf<P, Chain<Rest...>>::Expr::template App<HeadFilter>;
-};
+// // 2. Case for Recursive Sub-Chains within the hardware tree
+// template<typename P, typename... OO, typename... Rest>
+// struct FilterIf<P, Chain<Chain<OO...>, Rest...>> {
+//   using HeadFilter = typename FilterIf<P, Chain<OO...>>::Expr;
+//   using Expr = typename FilterIf<P, Chain<Rest...>>::Expr::template App<HeadFilter>;
+// };
 
 void run() {
   // 1. Direct Inspection Tests using IsInstanceOf
@@ -178,6 +179,8 @@ void run() {
 
   cout<<"Left Domain (Remaining infrastructure): ";
   Item<LeftDomain>::put();  // ACA
+
+  using X=typename Not<SameAs<B>>::template Apply<Item<A>>::X;
 }
 
 #ifdef ARDUINO
--- include/hapi/hapi.h
+++ include/hapi/hapi.h
@@ -78,12 +78,6 @@ namespace hapi {
     };
   };
 
-  /// @brief apply a predicate over a Chain (rules Chain query specialization)
-  /// @tparam Q : predicate
-  /// @tparam O : target object
-  template<typename Q,typename... OO>
-  constexpr const bool query<Q,Chain<OO...>>{(query<Q,OO>||...)};
-
   /// @brief predicate to select elements by type
   /// @tparam Q : the selector type
   template<typename Q> struct SameAs {
@@ -91,7 +85,113 @@ namespace hapi {
     template<typename O> struct Check {
       static constexpr const bool value{std::is_same_v<O,Q>};
     };
+  };
 
+  // Variadic Logical Predicates ----------------------------
+  template <typename P>
+  struct Not {
+    template <typename O>
+    struct Check {
+      static constexpr const bool value{!P::template Check<O>::value};
+    };
+  };
+
+  template <typename... Ps>
+  struct And {
+    template <typename O>
+    struct Check {
+      static constexpr const bool value{(Ps::template Check<O>::value && ... && true)};
+    };
+  };
+
+  template <typename... Ps>
+  struct Or {
+    template <typename O>
+    struct Check {
+      static constexpr const bool value{(Ps::template Check<O>::value || ... )};
+    };
+  };
+
+  // Monadic Channels (Concrete Control Metaprogramming) ----
+  template<typename T>
+  struct Left {
+    using Type = T;
+    template<typename O> struct Check { 
+      static constexpr bool value = std::is_same_v<O, Left<T>> || std::is_same_v<O, T>; 
+    };
+  };
+
+  template<typename T>
+  struct Right {
+    using Type = T;
+    template<typename O> struct Check { 
+      static constexpr bool value = std::is_same_v<O, Right<T>> || std::is_same_v<O, T>; 
+    };
+  };
+
+  // Universal Inspection Tool ------------------------------
+  template<template<typename...> class Wrapper>
+  struct IsInstanceOf {
+    template<typename O> struct Check { 
+      static constexpr bool value{false}; 
+    };
+    template<typename... Args> struct Check<Wrapper<Args...>> { 
+      static constexpr bool value{true}; 
+    };
+  };
+
+  // Global Convenience Aliases for the Toolset
+  struct IsLeft  : IsInstanceOf<Left> {};
+  struct IsRight : IsInstanceOf<Right> {};
+
+  // Categorization Transformation ---------------------------
+  template<typename Q>
+  struct Partition {
+    template<typename O>
+    struct Apply {
+      static constexpr bool value = Q::template Check<O>::value;
+      using Expr = std::conditional_t<value, Right<O>, Left<O>>;
+    };
+  };
+
+  // Pure 1:1 Map Topology Walker ---------------------------
+  template<typename F, typename O>
+  struct Map {
+    using Expr = typename F::template Apply<O>::Expr;
+  };
+
+  template<typename F, typename... OO>
+  struct Map<F, Chain<OO...>> {
+    using Expr = Chain<typename Map<F, OO>::Expr...>;
+  };
+
+  // Conditional Extraction and Cleanup Engine --------------
+  template<typename P, typename C, typename Enable = void> 
+  struct FilterIf;
+
+  template<typename P> 
+  struct FilterIf<P, Chain<>> { 
+    using Expr = Chain<>; 
+  };
+
+  // 1. Case for Monadic Wrappers (Excludes Chains using SFINAE to prevent ambiguity)
+  template<typename P, template<typename...> class Wrapper, typename T, typename... Args, typename... OO>
+  struct FilterIf<P, Chain<Wrapper<T, Args...>, OO...>, 
+                  std::enable_if_t<!std::is_same_v<Wrapper<T, Args...>, Chain<T, Args...>>>> {
+    static constexpr bool match = P::template Check<Wrapper<T, Args...>>::value;
+    
+    using Expr = std::conditional_t<
+      match,
+      typename FilterIf<P, Chain<OO...>>::Expr::template App<T>,
+      typename FilterIf<P, Chain<OO...>>::Expr
+    >;
+  };
+
+  // 2. Case for Recursive Sub-Chains within the hardware tree
+  template<typename P, typename... OO, typename... Rest>
+  struct FilterIf<P, Chain<Chain<OO...>, Rest...>> {
+    using HeadFilter = typename FilterIf<P, Chain<OO...>>::Expr;
+    using Expr = typename FilterIf<P, Chain<Rest...>>::Expr::template App<HeadFilter>;
   };
 
   // ====================== RULES DETECTION ======================--
@@ -163,6 +263,12 @@ namespace hapi {
   struct APIOf<API> : API { 
     using Base = API; 
     using Base::Base;
+  };
+
+  // Map specialization to traverse directly through an APIOf boundary
+  template<typename F, typename API, typename... OO>
+  struct Map<F, APIOf<API, OO...>> {
+    using Expr = APIOf<API, typename Map<F, OO>::Expr...>;
   };
 
   /// @brief provide circular reference to the whole chain if needed
